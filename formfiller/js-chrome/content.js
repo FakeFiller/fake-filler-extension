48d96ef0fe2f1d17c9720894ff7bfa568790674b
Josh Sklar <jrmsklar@gmail.com>
2015-04-08 11:45:20 -0400

    fill all inputs when keyboard shortcut pressed

--- formfiller/js-chrome/content.js
+++ formfiller/js-chrome/content.js
@@ -704,7 +704,23 @@ document.addEventListener('mousedown', function (event) {
     }
 });
 
+document.addEventListener('keydown', function (event) {
+    if (event.which == 222 && (event.metaKey || event.ctrlKey)) {
+    	
+    	chrome.runtime.sendMessage('getOptions', function (response) {
+	    	if (!window.formFiller) {
+	        	window.formFiller = new FormFiller(jQuery, response.options);
+	    	}
+
+	    	if (response.options.enableKeyboardShortcut) {
+				window.formFiller.fillAllInputs();
+			}
+		});
+    }
+});
+
 chrome.runtime.sendMessage('getOptions', function (response) {
+
     if (!window.formFiller) {
         window.formFiller = new FormFiller(jQuery, response.options);
     }
