36daf90519a7208b301af8df0863ad6b789c9306
Hussein Shabbir <hussein.a5@outlook.com>
2023-11-17 08:50:10 -0500

    Update changelog and version

--- src/options/components/ChangeLogPage.tsx
+++ src/options/components/ChangeLogPage.tsx
@@ -7,6 +7,13 @@ const ChangeLogPage = () => {
     <div>
       <h2>Fake Filler Changelog</h2>
       <p>
+        <strong>Version 3.4.0</strong>
+      </p>
+      <ul>
+        <li>Use manifest v3.</li>
+      </ul>
+      <hr />
+      <p>
         <strong>Version 3.3.0</strong>
       </p>
       <ul>
